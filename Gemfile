a43a0991a983170fa4bd2f91085bd92afa630e9a
Logical Groove <info@logicalgroove.com>
2014-10-16 16:10:19 +0000

    add referrals to admin view and IP fix

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :assets do
 end
 
 group :development do
+  gem 'byebug'
   gem 'pry'
 end
 
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register User do
   actions :index, :show
+
+  show do
+    attributes_table do
+      row :id
+      row :email
+      row :referral_code
+      row :referrer
+      row :created_at
+      row :updated_at
+      row :referrals do
+        user.referrals.blank? ? 'EMPTY' : user.referrals.map(&:email).join(', ')
+      end
+    end
+  end
 end
