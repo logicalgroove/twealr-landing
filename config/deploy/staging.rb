2a68f3a2ed8230434cb9b2792eaf2df27a56857b
Logical Groove <info@logicalgroove.com>
2014-10-22 09:01:07 +0000

    cleanup cap settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,8 @@
-server 'twealr.showsofa.com', user: 'dev', roles: %w{web app}
+server 'twealr.showsofa.com', roles: %w{web app db}
 set :rails_env, 'production'
 
 set :ssh_options, {
+  user: 'dev',
   keys: %w(/home/pair/.ssh/id_rsa_lg),
   forward_agent: true,
   auth_methods: %w(publickey)
